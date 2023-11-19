4565fb3a438df60e913868869562db6bcdd57d0e
Elizabeth <e.bellingham02@gmail.com>
2023-11-19 07:36:38 -0500

    added footer and link to open source on github

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        Open Sourced:
+        <a
+          href="https://github.com/e-bellingham/weather-react?search=1"
+          target="_blank"
+        >
+          github
+        </a>
+      </footer>
     </div>
   );
 }
